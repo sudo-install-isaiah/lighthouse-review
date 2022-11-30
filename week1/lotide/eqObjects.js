09d88c5b2f27f1810c6088714fb228994558b536
zae <isaiah.iu@hotmail.com>
2022-11-30 15:26:36 -0500

    eqObjects.js  to compare objects

---
+++ week1/lotide/eqObjects.js
@@ -0,0 +1,33 @@
+import { assertEqual } from "./assertEqual.js";
+import { assertNotEqual } from "./assertNotEqual.js";
+import { eqArrays } from "./eqArrays.js";
+
+export function eqObjects(obj1, obj2) {
+	// check for same number of keys
+	if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+		return false;
+	}
+	// loop first obj and run a check against obj2
+	for (const key in obj1) {
+		// if array, use eqArrays
+		if (Array.isArray(obj1[key])) {
+			if (eqArrays(obj1[key], obj2[key]) === false) {
+				return false;
+			}
+		} else if (obj1[key] !== obj2[key]) {
+			return false;
+		}
+		return true;
+	}
+}
+// TESTS
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+eqObjects(shirtObject, anotherShirtObject); // => true
+
+const longSleeveShirtObject = {
+	size: "medium",
+	color: "red",
+	sleeveLength: "long",
+};
+eqObjects(shirtObject, longSleeveShirtObject); // => false
