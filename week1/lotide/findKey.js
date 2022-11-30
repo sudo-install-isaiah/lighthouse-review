7f46d36e99ff5ac71f9dafc3d8b250fff1da5bfa
zae <isaiah.iu@hotmail.com>
2022-11-30 17:33:50 -0500

    Implement function that finds key by callback

---
+++ week1/lotide/findKey.js
@@ -0,0 +1,24 @@
+import { assertEqual } from "./assertEqual.js";
+
+export function findKey(object, callback) {
+	for (let key in object) {
+		if (callback(object[key])) {
+			return key;
+		}
+	}
+}
+
+assertEqual(
+	findKey(
+		{
+			"Blue Hill": { stars: 1 },
+			Akaleri: { stars: 3 },
+			noma: { stars: 2 },
+			elBulli: { stars: 3 },
+			Ora: { stars: 2 },
+			Akelarre: { stars: 3 },
+		},
+		x => x.stars === 2
+	),
+	"noma"
+);
