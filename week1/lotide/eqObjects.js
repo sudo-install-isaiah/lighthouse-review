ad684bfde1f2c7f62ea0fbf69f520a5da570618e
zae <isaiah.iu@hotmail.com>
2022-11-30 15:57:52 -0500

    Refactoring complete, comments added

--- week1/lotide/eqObjects.js
+++ week1/lotide/eqObjects.js
@@ -3,26 +3,24 @@ import { eqArrays } from "./eqArrays.js";
 
 export function eqObjects(obj1, obj2) {
 	// check for same number of keys
-	if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-		return false;
-	}
-	// loop first obj and run a check against obj2
-	for (const key in obj1) {
-		// if array, use eqArrays
-		if (Array.isArray(obj1[key])) {
-			if (eqArrays(obj1[key], obj2[key]) === false) {
-				return false;
-			}
-		} else if (obj1[key] !== obj2[key]) {
-			return false;
+	if (Object.keys(obj1).length === Object.keys(obj2).length) {
+		// loop first obj and run a check against obj2
+		for (const key in obj1) {
+			// compare both key value pairs and store into result
+			let result = Array.isArray(obj1[key])
+				? eqArrays(obj1[key], obj2[key])
+				: obj1[key] === obj2[key];
+			if (!result) return false;
 		}
+		//if reaches the end of the loop, return true
 		return true;
 	}
+	return false;
 }
 // TESTS
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
-eqObjects(shirtObject, anotherShirtObject); // => true
+// eqObjects(shirtObject, anotherShirtObject); // => true
 //We need to use that return value in combination with assertEqual to test if the function is working correctly.
 assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
 
@@ -31,5 +29,5 @@ const longSleeveShirtObject = {
 	color: "red",
 	sleeveLength: "long",
 };
-eqObjects(shirtObject, longSleeveShirtObject); // => false
+// eqObjects(shirtObject, longSleeveShirtObject); // => false
 assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
