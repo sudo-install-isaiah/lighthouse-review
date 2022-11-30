e939a9b397fe8d394a538729c6385cac3732bb1e
zae <isaiah.iu@hotmail.com>
2022-11-30 14:28:30 -0500

    countLetters.js passes tests

--- week1/lotide/countLetters.js
+++ week1/lotide/countLetters.js
@@ -6,7 +6,7 @@ export function countLetters(string) {
 	// remove spaces and convert case
 	const str = string.split(" ").join("").toLowerCase();
 	// loop string
-	for (letter of str) {
+	for (const letter of str) {
 		// if letter in result exists, ++, else define
 		result[letter] ? result[letter]++ : (result[letter] = 1);
 	}
@@ -14,15 +14,17 @@ export function countLetters(string) {
 	return result;
 }
 
-assertEqual(countLetters("lighthouse in the house"), {
-	l: 1,
-	i: 2,
-	g: 1,
-	h: 4,
-	t: 2,
-	o: 2,
-	u: 2,
-	s: 2,
-	e: 3,
-	n: 1,
-});
+// assertEqual(countLetters("lighthouse in the house"), {
+// 	l: 1,
+// 	i: 2,
+// 	g: 1,
+// 	h: 4,
+// 	t: 2,
+// 	o: 2,
+// 	u: 2,
+// 	s: 2,
+// 	e: 3,
+// 	n: 1,
+// });
+
+// console.log(countLetters("lighthouse in the house"));
