fa8092a37a8c680542ab739887afd42476b36609
zae <isaiah.iu@hotmail.com>
2022-11-30 13:51:07 -0500

    Change export in assertEqual, countOnly.js passes tests

--- week1/lotide/assertEqual.js
+++ week1/lotide/assertEqual.js
@@ -1,12 +1,10 @@
 // FUNCTION IMPLEMENTATION
 
-const assertEqual = function (actual, expected) {
+export function assertEqual(actual, expected) {
 	actual === expected
 		? console.log(`Assertion Passed: ${actual} === ${expected}`)
 		: console.log(`Assertion Failed: ${actual} !== ${expected}`);
-};
+}
 
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1, 1);
-
-export default assertEqual;
