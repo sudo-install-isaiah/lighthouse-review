5be8f30c0b93d30118331abfd67f06b020c188fb
zae <isaiah.iu@hotmail.com>
2022-11-30 14:23:41 -0500

    countLetters.js ready for testing

---
+++ week1/lotide/countLetters.js
@@ -0,0 +1,28 @@
+import { assertEqual } from "./assertEqual.js";
+
+export function countLetters(string) {
+	// define result object
+	const result = {};
+	// remove spaces and convert case
+	const str = string.split(" ").join("").toLowerCase();
+	// loop string
+	for (letter of str) {
+		// if letter in result exists, ++, else define
+		result[letter] ? result[letter]++ : (result[letter] = 1);
+	}
+	// return result object
+	return result;
+}
+
+assertEqual(countLetters("lighthouse in the house"), {
+	l: 1,
+	i: 2,
+	g: 1,
+	h: 4,
+	t: 2,
+	o: 2,
+	u: 2,
+	s: 2,
+	e: 3,
+	n: 1,
+});
